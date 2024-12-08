8c5748c21355a5844ab67f0bc1f0426ccafe8d6e
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-08 23:24:30 +0200

    fix: fix send-article endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { webhookCallback } from './deps.deno.ts';
+import { getRandomNumber } from 'npm:ameliance-scripts';
 
 import { bot } from './bot.ts';
 import { connectToDatabase } from './libs/db/mongoose.ts';
@@ -10,21 +11,21 @@ Deno.serve(async (req: Request) => {
 	await connectToDatabase();
 	const url = new URL(req.url);
 
-	if (req.method === 'POST') {
-		if (url.pathname.slice(1) === bot.token) {
-			try {
-				return await handleUpdate(req);
-			} catch (err) {
-				console.error(err);
-			}
+	if (req.method === 'POST' && url.pathname.slice(1) === bot.token) {
+		try {
+			return await handleUpdate(req);
+		} catch (err) {
+			console.error(err);
+			return new Response('Error', { status: 500 });
 		}
 	}
 
 	if (req.method === 'GET' && url.pathname === '/send-article') {
 		try {
 			await sendArticle(bot.context);
-			console.log('Article sent successfully');
-			return new Response('OK', { status: 200 });
+			const nextInterval = getRandomNumber(1, 5);
+			console.log(`Article sent successfully. Next interval: ${nextInterval} minutes`);
+			return new Response(String(nextInterval), { status: 200 });
 		} catch (error) {
 			console.error('Error during article sending:', error);
 			return new Response('Error', { status: 500 });
