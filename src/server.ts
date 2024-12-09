8cf282f376949704ba6f63926ff9102475bebdcc
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-09 14:47:23 +0200

    fix: fix send-article endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { webhookCallback } from './deps.deno.ts';
+import { getRandomNumber } from 'npm:ameliance-scripts';
 
 import { bot } from './bot.ts';
 import { connectToDatabase } from './libs/db/mongoose.ts';
@@ -25,8 +26,32 @@ Deno.serve(async (req: Request) => {
 		if (url.pathname === '/send-article') {
 			console.log('send-article: ', true);
 			try {
-				await sendArticle();
-				return new Response('OK', { status: 200 });
+				const randomMinutes = getRandomNumber(1, 5);
+				const lastExecution = Number(Deno.env.get('LAST_EXECUTION') || 0);
+				const now = Date.now();
+
+				const nextExecutionTime = new Date(now + randomMinutes * 60 * 1000);
+
+				if (now - lastExecution >= randomMinutes * 60 * 1000) {
+					console.log('Executing sendArticle');
+					await sendArticle();
+					Deno.env.set('LAST_EXECUTION', now.toString());
+					return new Response(
+						`OK. Next article will be sent at ${nextExecutionTime.toLocaleTimeString(
+							'en-US',
+							{
+								hour: '2-digit',
+								minute: '2-digit',
+							},
+						)}`,
+						{ status: 200 },
+					);
+				} else {
+					console.log('Skipping execution');
+					return new Response('Skipped', { status: 200 });
+				}
+				// await sendArticle();
+				// return new Response('OK', { status: 200 });
 			} catch (error) {
 				console.error('Error during post sending:', error);
 				return new Response('Error', { status: 500 });
