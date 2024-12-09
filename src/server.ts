620a423ca85c19d3e0d880279b8377e4004bf585
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-09 13:04:54 +0200

    chore: test

--- src/server.ts
+++ src/server.ts
@@ -11,31 +11,36 @@ Deno.serve(async (req: Request) => {
 	await connectToDatabase();
 	const url = new URL(req.url);
 
-	if (req.method === 'POST' && url.pathname.slice(1) === bot.token) {
-		try {
-			return await handleUpdate(req);
-		} catch (err) {
-			console.error(err);
-			return new Response('Error', { status: 500 });
+	if (req.method === 'POST') {
+		if (url.pathname.slice(1) === bot.token) {
+			try {
+				return await handleUpdate(req);
+			} catch (err) {
+				console.error(err);
+				return new Response('Error', { status: 500 });
+			}
 		}
 	}
 
-	if (req.method === 'GET' && url.pathname === '/send-article') {
-		console.log('url: ', url);
-		try {
-			const ctx = await bot.createContext({
-				update: {},
-				api: bot.api,
-				me: await bot.api.getMe(),
-			});
-
-			await sendArticle(ctx);
-			const nextInterval = getRandomNumber(1, 5);
-			console.log(`Article sent successfully. Next interval: ${nextInterval} minutes`);
-			return new Response(String(nextInterval), { status: 200 });
-		} catch (error) {
-			console.error('Error during article sending:', error);
-			return new Response('Error', { status: 500 });
+	if (req.method === 'GET') {
+		console.log('GET url: ', url);
+		if (url.pathname === '/send-article') {
+			console.log('send-article: ', true);
+			try {
+				// 	const ctx = await bot.createContext({
+				// 		update: {},
+				// 		api: bot.api,
+				// 		me: await bot.api.getMe(),
+				// 	});
+				// 	const api = new Api(bot.token);
+				// 	await sendArticle(api.);
+				// 	const nextInterval = getRandomNumber(1, 5);
+				// 	console.log(`Article sent successfully. Next interval: ${nextInterval} minutes`);
+				// 	return new Response(String(nextInterval), { status: 200 });
+			} catch (error) {
+				console.error('Error during article sending:', error);
+				return new Response('Error', { status: 500 });
+			}
 		}
 	}
 
