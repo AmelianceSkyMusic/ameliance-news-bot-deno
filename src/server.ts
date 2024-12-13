29ae289e4360f88641235f170f0d0c711d95b907
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2024-12-13 19:03:01 +0200

    refactor: remove next post info message

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import { webhookCallback } from './deps.deno.ts';
 import { getRandomNumber } from 'npm:ameliance-scripts';
+import { webhookCallback } from './deps.deno.ts';
 
+import { sendArticle } from './actions/helpers/send-article.ts';
 import { bot } from './bot.ts';
 import { connectToDatabase } from './libs/db/mongoose.ts';
-import { sendArticle } from './actions/helpers/send-article.ts';
-import { ENV } from './constants/env.ts';
 
 const handleUpdate = webhookCallback(bot, 'std/http');
 
@@ -28,20 +27,12 @@ Deno.serve(async (req: Request) => {
 			try {
 				const fiveMinInMs = 5 * 60 * 1000;
 				const randomDelay = getRandomNumber(0, fiveMinInMs);
-				const now = Date.now();
-				const nextExecutionTime = new Date(now + randomDelay);
-				const nextExecutionTimeIn24Format = nextExecutionTime.toLocaleTimeString('en-US', {
-					hour: '2-digit',
-					minute: '2-digit',
-					hour12: false,
-					timeZone: 'Europe/Kiev',
-				});
-				const nextExecutionMessage =
-					`OK. Next article will be sent at ${nextExecutionTimeIn24Format}`;
+
 				setTimeout(async () => {
-					await sendArticle(nextExecutionMessage);
+					await sendArticle();
 				}, randomDelay);
-				return new Response(nextExecutionMessage, { status: 200 });
+
+				return new Response('Ok', { status: 200 });
 			} catch (error) {
 				console.error('Error during post sending:', error);
 				return new Response('Error', { status: 500 });
