1c4a631139adf3c6720566b107e4c6d5594d1a6c
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2025-03-19 17:12:18 +0200

    feat: add send different media in news message

--- src/actions/on/message.ts
+++ src/actions/on/message.ts
@@ -1,3 +1,9 @@
+import {
+	InputMediaAudio,
+	InputMediaDocument,
+	InputMediaPhoto,
+	InputMediaVideo,
+} from 'https://deno.land/x/grammy@v1.32.0/types.ts';
 import { bot } from '../../bot.ts';
 import { ENV } from '../../constants/env.ts';
 import { Context, handleAppError, hasAccess, logUserInfo } from '../../deps.deno.ts';
@@ -11,10 +17,25 @@ export function message() {
 			logUserInfo(ctx, { message: 'on message', accessMessage: hasAccessToRunCommand });
 			if (!hasAccessToRunCommand) return;
 
-			const text = ctx.msg.caption;
+			const text = ctx.msg?.caption;
 
-			const postPhoto = ctx.msg.photo?.[0].file_id || '';
-			if (!postPhoto) return;
+			const postMediaGroup = ctx.msg?.media_group_id || '';
+			const postPhoto = ctx.msg?.photo?.at(-1)?.file_id || ''; //* Get last photo from photo array with different sizes
+			const postDocument = ctx.msg?.document?.file_id || '';
+			const postVideo = ctx.msg?.video?.file_id || '';
+			const postGif = ctx.msg?.animation?.file_id || '';
+			const postAudio = ctx.msg?.audio?.file_id || '';
+
+			if (
+				!postMediaGroup &&
+				!postPhoto &&
+				!postDocument &&
+				!postVideo &&
+				!postGif &&
+				!postAudio
+			) {
+				return;
+			}
 
 			const articleText = text?.split('\n').splice(1).join('\n').trim();
 			if (!articleText) return;
@@ -22,10 +43,82 @@ export function message() {
 			const postAsHTML = await generateBimbaPostAsHTML({ text: articleText });
 			if (!postAsHTML) return;
 
-			await ctx.api.sendPhoto(Number(ENV.BIMBA_NEWS_ID), postPhoto, {
-				caption: postAsHTML,
-				parse_mode: 'HTML',
-			});
+			if (postMediaGroup) {
+				const mediaGroup: Array<
+					InputMediaAudio | InputMediaDocument | InputMediaPhoto | InputMediaVideo
+				> = [];
+
+				if (ctx.msg?.media_group_id) {
+					const messages = await ctx.api.getUpdates();
+					let firstItemAdded = false;
+					for (const message of messages) {
+						if (message.message?.media_group_id === ctx.msg.media_group_id) {
+							if (message.message.video) {
+								mediaGroup.push({
+									type: 'video',
+									media: message.message.video.file_id,
+									caption: !firstItemAdded ? postAsHTML : undefined,
+									parse_mode: !firstItemAdded ? 'HTML' : undefined,
+								});
+								firstItemAdded = true;
+							} else if (message.message.photo) {
+								mediaGroup.push({
+									type: 'photo',
+									media: message.message.photo?.at(-1)?.file_id || '',
+									caption: !firstItemAdded ? postAsHTML : undefined,
+									parse_mode: !firstItemAdded ? 'HTML' : undefined,
+								});
+								firstItemAdded = true;
+							} else if (message.message.document) {
+								mediaGroup.push({
+									type: 'document',
+									media: message.message.document.file_id,
+									caption: !firstItemAdded ? postAsHTML : undefined,
+									parse_mode: !firstItemAdded ? 'HTML' : undefined,
+								});
+								firstItemAdded = true;
+							} else if (message.message.audio) {
+								mediaGroup.push({
+									type: 'audio',
+									media: message.message.audio.file_id,
+									caption: !firstItemAdded ? postAsHTML : undefined,
+									parse_mode: !firstItemAdded ? 'HTML' : undefined,
+								});
+								firstItemAdded = true;
+							}
+						}
+					}
+				}
+
+				if (mediaGroup.length > 0) {
+					await ctx.api.sendMediaGroup(Number(ENV.BIMBA_NEWS_ID), mediaGroup);
+				}
+			} else if (postPhoto) {
+				await ctx.api.sendPhoto(Number(ENV.BIMBA_NEWS_ID), postPhoto, {
+					caption: postAsHTML,
+					parse_mode: 'HTML',
+				});
+			} else if (postDocument) {
+				await ctx.api.sendDocument(Number(ENV.BIMBA_NEWS_ID), postDocument, {
+					caption: postAsHTML,
+					parse_mode: 'HTML',
+				});
+			} else if (postVideo) {
+				await ctx.api.sendVideo(Number(ENV.BIMBA_NEWS_ID), postVideo, {
+					caption: postAsHTML,
+					parse_mode: 'HTML',
+				});
+			} else if (postGif) {
+				await ctx.api.sendAnimation(Number(ENV.BIMBA_NEWS_ID), postGif, {
+					caption: postAsHTML,
+					parse_mode: 'HTML',
+				});
+			} else if (postAudio) {
+				await ctx.api.sendAudio(Number(ENV.BIMBA_NEWS_ID), postAudio, {
+					caption: postAsHTML,
+					parse_mode: 'HTML',
+				});
+			}
 		} catch (error) {
 			await handleAppError(ctx, error);
 		}
